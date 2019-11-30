b9a840450011d4f7ae14b09f90176c32395e2f8c
Nico Esposito <nico.esposito@gmail.com>
2019-11-30 18:44:15 -0300

    completed 466

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,13 +7,44 @@ import Search from './Search';
 const Ingredients = () => {
   const [ userIngredients, setUserIngredients ] = useState([]);
 
+  useEffect(() => {
+    fetch(
+      "https://reacthooks-f960d.firebaseio.com/ingredients.json").then(response =>
+        response.json().then(responseData => {
+          const loadedIngredients = [];
+          for (const key in responseData) {
+            loadedIngredients.push({
+              id: key,
+              title: responseData[key].title,
+              amount: responseData[key].amount
+            });
+          }
+          setUserIngredients(loadedIngredients);
+        })
+      )
+  }, []);
+
+  useEffect(() => {
+    console.log('Rendering Ingredients', userIngredients)
+  }, [userIngredients] );
+      
+
   const addIngredientHandler = ingredient => {
-    
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient }
-    ]);
-  }
+    fetch("https://reacthooks-f960d.firebaseio.com/ingredients.json", {
+      method: "POST",
+      body: JSON.stringify(ingredient),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(dataResponse =>
+        setUserIngredients(prevIngredients => [
+          ...prevIngredients,
+          { id: dataResponse.name, ...ingredient }
+        ])
+      );
+  };
 
   const removeIngredientHandler = ingredient => { 
     }
